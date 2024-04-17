737b79e234280bc2f2765affbbaafe71f5370dd5
kaetlynbsanders <kaetlynsanders@gmail.com>
2024-04-17 18:01:17 -0500

    Initial push to repo of our color console.

---
+++ index.js
@@ -0,0 +1,5 @@
+import { color, log, red, green} from 'console-log-colors';
+
+console.log(color.red("This is a really really boring message."));
+console.log(color.green("This is not a boring message"));
+
